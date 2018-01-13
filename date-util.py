499954cf090c9b7b4dfb835942f57413c3c88f67
sachinkulshrestha <sacvijai@gmail.com>
2018-01-13 16:59:16 +0530

    merge resolved

--- date-util.py
+++ date-util.py
@@ -2,12 +2,8 @@
 
 # function that returns only the current year
 
-<<<<<<< HEAD
-# function that return the year and month
 
-# function that return the Geogorian Date
-=======
 # function that will return the time stamp
 
 # function that will return the current month
->>>>>>> master
+
