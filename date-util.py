cd0f865f2bfec3e3522002cceb59ddb13ed4a0ca
sachinkulshrestha <sacvijai@gmail.com>
2018-01-13 17:03:07 +0530

    added current day time of the week

--- date-util.py
+++ date-util.py
@@ -2,8 +2,8 @@
 
 # function that returns only the current year
 
-
 # function that will return the time stamp
 
 # function that will return the current month
 
+# function that will return me the current day of the week (sunday-sat)
