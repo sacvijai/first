68385031c5bace818201767c012f3a80f1c3ae26
sachinkulshrestha <sacvijai@gmail.com>
2018-01-13 16:41:39 +0530

    Remove Hello Word

--- date-util.py
+++ date-util.py
@@ -1,3 +1,7 @@
 # function that returns only the current date
 
 # function that returns only the current year
+
+# function that will return the time stamp
+
+# function that will return the current month
