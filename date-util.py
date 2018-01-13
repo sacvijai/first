982f7335f27730b7ae65a05f929c9f2fff9d4c5e
sachinkulshrestha <sacvijai@gmail.com>
2018-01-13 16:49:52 +0530

    Added the year and month and Geogorian Date

--- date-util.py
+++ date-util.py
@@ -1,3 +1,7 @@
 # function that returns only the current date
 
 # function that returns only the current year
+
+# function that return the year and month
+
+# function that return the Geogorian Date
